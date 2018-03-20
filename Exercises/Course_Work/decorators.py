153c38893b26312b70ed8fedc262c75d59327edc
Kyrene Jackson <ky.jackson01@gmail.com>
2018-03-20 11:20:37 -0500

    Add decorator notes

---
+++ Exercises/Course_Work/decorators.py
@@ -0,0 +1,34 @@
+import functools
+
+def my_decorator(func):
+    @functools.wraps(func)
+    def function_that_runs_func():
+        print("In the decorator!")
+        func()
+        print("After the decorator!")
+    return function_that_runs_func
+
+
+@my_decorator
+def my_function():
+    print("I'm the function!")
+
+# my_function()
+
+##
+
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_that_runs_func():
+            print("In the decorator!")
+            func()
+            print("After the decorator!")
+        return function_that_runs_func
+    return my_decorator
+
+@decorator_with_arguments(56)
+def my_function_too():
+    print("Hello")
+
+my_function_too
