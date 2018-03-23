985d6a3d6a4a138f238b24c36a7ec46fdbb9f06a
Kyrene Jackson <ky.jackson01@gmail.com>
2018-03-22 21:45:48 -0500

    Complete guessing game challenge mvp, add runner

--- Exercises/Challenges/guessing_game.py
+++ Exercises/Challenges/guessing_game.py
@@ -18,44 +18,67 @@ class Game:
 
     def __init__(self, player):
         self.player = player
+        self.current_guess = None
         self.mystery_number = random.randint(1, 100)
 
     def start_game(self):
         print(Game.RULES)
 
-    def is_in_range(self, player_number, mystery_number):
-        return abs(mystery_number - player_number) in range(1, 11)
+    def get_valid_player_guess(self):
+        while True:
+            try:
+                player_input = int(input("What is my number? "))
+
+            except ValueError:
+                print("Oops! That was not a valid number, try again...")
+                continue
+
+            if self.valid_guess(player_input):
+                self.current_guess = player_input
+                return self.current_guess
+
+            else:
+                print("OUT OF BOUNDS!")
+
+    def valid_guess(self, number):
+        if 1 <= number <= 100:
+            return True
+        return False
+
+    def add_to_guess_list(self, guess_list):
+        return guess_list.append(self.current_guess)
+
+    def correct_number(self):
+        return self.current_guess == self.mystery_number
+
+    def is_in_range(self, player_number):
+        return abs(self.mystery_number - player_number) in range(1, 11)
 
     def play_round(self):
         pass
 
-    def get_previous_guess(self, guess_list):
+    def previous_guess(self, guess_list):
         return guess_list[-1]
 
     def game_stats(self, guess_list):
         for index, value in enumerate(guess_list, start=1):
             print('On turn {} you guessed {}'.format(index, value))
 
-    def next_round(self, guess_list):
-        pass
-
-
-
-
+    def current_status(self, guess_list):
+        if abs(self.mystery_number - self.current_guess) < abs(self.mystery_number - self.previous_guess(guess_list)):
+            return True
+        return False
 
 
 class Player:
 
     def __init__(self):
-        self.guesses = []
+        self.guesses = [0]
 
-    def is_first_round(self):
-        return len(self.guesses) == 0
+    def first_round(self):
+        return len(self.guesses) == 1
 
-    def add_to_guess_list(self, number):
-        return self.guesses.append(number)
-
-    def get_total_guess_count(self):
+    def total_guess_count(self):
         return len(self.guesses)
 
 
--- Exercises/Challenges/runner.py
+++ Exercises/Challenges/runner.py
@@ -1,22 +1,58 @@
 from guessing_game import Game
 from guessing_game import Player
 
-# player = Player()
-# game = Game(player)
-# my_number = 7
-# mystery_number = game.mystery_number
-#
-# print("This is the first round: {}".format(player.is_first_round()))
-# print("Mystery Number: {}".format(game.mystery_number))
-# print("My Number: {}".format(my_number))
-# print("In range?: {}".format(game.is_in_range(my_number, mystery_number)))
-#
-# player.add_to_guess_list(12)
-# player.add_to_guess_list(20)
-# game.next_round(player.guesses)
+# STEPS
+# Show rules
+# Get valid player guess
+# Add valid guess to guess list
+# Determine if this is the player's first round
+    # IF first round
+        # IF in range
+            # WARM!
+        # ELSE
+            # COLD!
+    # Add guess to guess list
+
+    # IF not first round
+        # IF guess is closer to the number than previous guess
+
+
 
 player = Player()
 game = Game(player)
 
 
 game.start_game()
+print(game.mystery_number)
+game.get_valid_player_guess()
+
+while not game.correct_number():
+
+    if player.first_round():
+        if game.is_in_range(game.current_guess):
+            print("WARM!")
+        else:
+            print("COLD!")
+
+    if not player.first_round():
+        game.get_valid_player_guess()
+
+        if game.current_status(player.guesses):
+            print("WARMER!")
+        else:
+            print("COLDER!")
+
+    game.add_to_guess_list(player.guesses)
+
+print("You got it! It took you {} try(s)".format(player.total_guess_count()))
+
+
+
+
+
+
+
+
+
+
+
