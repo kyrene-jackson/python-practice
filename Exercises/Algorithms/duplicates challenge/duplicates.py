06c14471793289f5038b89bfbbde8c74b7f5295a
Kyrene Jackson <ky.jackson01@gmail.com>
2018-03-25 19:14:31 -0500

    Create file for unit tests

---
+++ Exercises/Algorithms/duplicates challenge/duplicates.py
@@ -0,0 +1,18 @@
+# Count the number of Duplicates
+#
+# Write a function that will return
+# the count of distinct case-insensitive
+# alphabetic characters and numeric digits
+# that occur more than once in the input string.
+# The input string can be assumed to contain
+# only alphabets (both uppercase and lowercase)
+# and numeric digits.
+#
+# Example: "aabBcde" -> 2
+# 'a' occurs twice and 'b' twice (bandB)
+#
+
+
+
+def duplicate_count(text):
+    # Your code goes here
