15a451b5b162c6540b96284fc308998764ab48de
Kyrene Jackson <ky.jackson01@gmail.com>
2018-03-28 13:29:45 -0500

    Complete disemvowel challenge

--- Exercises/Algorithms/disemvowel.py
+++ Exercises/Algorithms/disemvowel.py
@@ -6,3 +6,23 @@
 # "Ths wbst s fr lsrs LL!".
 # Note: for this kata y isn't considered a vowel.
 
+from unittest import TestCase
+
+
+def disemvowel(string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    string_list = list(string)
+
+    vowels_in_string = [letter for letter in string if letter.lower() in vowels]
+
+    for letter in string_list:
+        if letter in vowels_in_string:
+            string_list.remove(letter)
+    return "".join(string_list)
+
+
+class DisemvowelTest(TestCase):
+    def test_disemvowel(self):
+        self.assertEqual(disemvowel("This website is for losers LOL!"),
+                           "Ths wbst s fr lsrs LL!")
